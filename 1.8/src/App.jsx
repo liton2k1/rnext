3ca71898a31f2cce7b7ff1b1b98ddfd7057c28e2
liton2k1 <litonmia2k1@gmail.com>
2025-11-12 10:29:04 +0600

    module: 1.8 done

---
+++ 1.8/src/App.jsx
@@ -0,0 +1,14 @@
+import Avatar from "./components/Avatar";
+import Profile from "./components/Profile";
+
+function App() {
+  return (
+    <div>
+      <Profile>
+        <Avatar />
+      </Profile>
+    </div>
+  );
+}
+
+export default App;
---
+++ 1.8/src/components/Info.jsx
@@ -0,0 +1,7 @@
+export default function Info() {
+  return (
+    <div>
+      <h1>This is info component</h1>
+    </div>
+  );
+}
